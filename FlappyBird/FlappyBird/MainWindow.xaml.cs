934289b4665f456f551a318be1d5b56b5bc11804
Salty-Salter <65321289+Salty-Salter@users.noreply.github.com>
2020-08-26 20:13:59 +0800

    lots of good stuff

--- FlappyBird/FlappyBird/MainWindow.xaml.cs
+++ FlappyBird/FlappyBird/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Runtime.InteropServices;
 using System.IO;
 using System.Windows.Threading;
 using System.Globalization;
+using System.Windows.Forms;
 
 namespace FlappyBird
 {
@@ -25,45 +26,96 @@ namespace FlappyBird
     /// </summary>
     public partial class MainWindow : Window
     {
+        double gravity = 5;
+        int num = 0;
+        bool alive = true;
         public MainWindow()
         {
             InitializeComponent();
 
-            DispatcherTimer LiveTime = new DispatcherTimer();
-            LiveTime.Interval = TimeSpan.FromSeconds(0.002);
-            LiveTime.Tick += new EventHandler(gameTimer_Tick);
-            LiveTime.Start();
+            if (alive == true)
+            {
+                DispatcherTimer LiveTime = new DispatcherTimer();
+                LiveTime.Interval = TimeSpan.FromSeconds(0.002);
+                LiveTime.Tick += new EventHandler(gameTimer_Tick);
+                LiveTime.Start();
+            }
         }
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
+            num = num - 1;
+            Thickness icon = bird.Margin;
+            bird.Margin = new Thickness(icon.Left, icon.Top + gravity, 0, 0);
+
+
             Thickness marg = pipe1.Margin;
             Thickness marg1 = pipe2.Margin;
-            if (marg.Left != -100)
+            if (marg.Left >= -85)
             {
-                pipe1.Margin = new Thickness(marg.Left-1, 0, 0, 0);
-                pipe2.Margin = new Thickness(marg.Left - 1, marg1.Top,0,0);
+                pipe1.Margin = new Thickness(marg.Left-1.5, 0, 0, 0);
+                pipe2.Margin = new Thickness(marg.Left - 1.5, marg1.Top,0,0);
             }
             else
             {
-                marg.Left = 400;
+                marg.Left = 485;
                 pipe1.Margin = new Thickness(marg.Left, 0, 0, 0);
 
                 var random = new Random();
-                int num = random.Next(450);
+                int num = random.Next(350);
                 pipe1.Height = num;
-                pipe2.Margin = new Thickness(marg.Left, 0 + num + 100, 0, 0);
+                pipe2.Margin = new Thickness(marg.Left, 0 + num + 125, 0, 0);
 
-                if (500-num-100 > 0)
+                if (500-num-125 > 0)
                 { 
-                    pipe2.Height = 500 - num - 100; 
+                    pipe2.Height = 500 - num - 125; 
                 }
                 else
                 {
                     pipe2.Height = 0.01;
                 }
-
             }
+
+            Thickness marg3 = pipe3.Margin;
+            Thickness marg4 = pipe4.Margin;
+            if (marg3.Left >= -85)
+            {
+                pipe3.Margin = new Thickness(marg3.Left - 1.5, 0, 0, 0);
+                pipe4.Margin = new Thickness(marg3.Left - 1.5, marg4.Top, 0, 0);
+            }
+            else
+            {
+                marg3.Left = 485;
+                pipe3.Margin = new Thickness(marg3.Left, 0, 0, 0);
+
+                var random = new Random();
+                int num = random.Next(350);
+                pipe3.Height = num;
+                pipe4.Margin = new Thickness(marg3.Left, 0 + num + 125, 0, 0);
+
+                if (500 - num - 125 > 0)
+                {
+                    pipe4.Height = 500 - num - 125;
+                }
+                else
+                {
+                    pipe4.Height = 0.01;
+                }
+            }
+
+            if (num <= 0) { gravity = 5; }
+            else { gravity = gravity + 1.25; }
+        }
+
+        private void Jump(object sender, EventArgs e)
+        {
+            gravity = -15;
+            num = 10;
+        }
+
+        private void birdJump_Tick(object sender, EventArgs e)
+        {
+            
         }
     }
 }
